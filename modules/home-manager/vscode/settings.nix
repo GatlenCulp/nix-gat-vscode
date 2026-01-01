b7d91b25f9f89c519ef6604940deff371a9f1e16
GatlenCulp <GatlenCulp@gmail.com>
2025-12-31 21:31:08 -0500

    feat: make vscode terminal init to nushell

--- modules/home-manager/vscode/settings.nix
+++ modules/home-manager/vscode/settings.nix
@@ -255,12 +255,31 @@ let
       "terminal.external.osxExec" = "Ghostty.app";
       "terminal.integrated.cursorBlinking" = true;
       "terminal.integrated.cursorStyle" = "line";
-      "terminal.integrated.defaultProfile.osx" = "Gatlen";
       "terminal.integrated.enableImages" = true;
       "terminal.integrated.hideOnStartup" = "whenEmpty";
       "terminal.integrated.lineHeight" = 1.05;
+      "terminal.integrated.rightClickBehavior" = "default";
+      "terminal.integrated.shellIntegration.enabled" = true;
+      "terminal.integrated.smoothScrolling" = true;
+      "terminal.integrated.splitCwd" = "workspaceRoot";
+      "terminal.integrated.stickyScroll.enabled" = true;
+      "terminal.integrated.suggest.enabled" = true;
+      "terminal.integrated.tabs.enabled" = true;
+      "terminal.integrated.tabs.location" = "left";
+      # PROFILE SETTINGS
+      "terminal.integrated.defaultProfile.osx" = "Gatlen-nu";
       "terminal.integrated.profiles.osx" = {
-        "Gatlen" = {
+        "Gatlen-zsh" = {
+          "args" = [ "-l" ];
+          "color" = "terminal.ansiBlue";
+          "env" = {
+            "value" = "\${workspaceFolder}/secrets/.env";
+          };
+          "icon" = "robot";
+          "overrideName" = true;
+          "path" = "zsh";
+        };
+        "Gatlen-nu" = {
           "args" = [ "-l" ];
           "color" = "terminal.ansiBlue";
           "env" = {
@@ -271,14 +290,6 @@ let
           "path" = "zsh";
         };
       };
-      "terminal.integrated.rightClickBehavior" = "default";
-      "terminal.integrated.shellIntegration.enabled" = true;
-      "terminal.integrated.smoothScrolling" = true;
-      "terminal.integrated.splitCwd" = "workspaceRoot";
-      "terminal.integrated.stickyScroll.enabled" = true;
-      "terminal.integrated.suggest.enabled" = true;
-      "terminal.integrated.tabs.enabled" = true;
-      "terminal.integrated.tabs.location" = "left";
     };
 
     git = {
